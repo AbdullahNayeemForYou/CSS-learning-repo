da7fa113aa1dab0d0851f33322b7006f6683e80d
sadpotato5143 <psycho202234@gmail.com>
2025-06-21 09:45:12 +0600

    Completed the course

---
+++ Chapter 13-Position/script.js
@@ -0,0 +1,8 @@
+
+
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close-btn");
+
+closeBtn.onclick = function(){
+    return modal.style.display = "none";
+}
